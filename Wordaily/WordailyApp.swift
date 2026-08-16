da65c125a1bdeeb0f012f01e7f63e428dc31a8d5
sss-sara <sassac@familiamelo.com>
2026-08-16 15:12:03 -0300

    Initial Commit

---
+++ Wordaily/WordailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordailyApp.swift
+//  Wordaily
+//
+//  Created by Sara Melo on 16/08/26.
+//
+
+import SwiftUI
+
+@main
+struct WordailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
